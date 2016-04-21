814147e6ff46d1ce1730b7b4e36864db0fa1adbe
Vijay Weemhoff <vijay.w@hotmail.com>
2016-04-21 17:48:52 +0200

    pitches in array (error at 13 pitches)

--- melodic_main.cpp
+++ melodic_main.cpp
@@ -6,7 +6,12 @@ using namespace std;
 int numElements=4;
 int* lijst= new int[numElements];
 
+int aantal=0;
+int* pitches= new int[aantal];
+
 int velocityOSC;
+int pitchOSC;
+int showPitch;
 
 // subclass OSC into a local class so we can provide our own callback
 class localOSC : public OSC
@@ -19,6 +24,14 @@ class localOSC : public OSC
     if(!msgpath.compare("/velocity")){
       velocityOSC = argv[0]->i;
       //cout << "Message: " << velocityOSC << fixed << std::endl;
+    } // if
+    if(!msgpath.compare("/kslider_pitch")){
+       pitchOSC = argv[0]->i;
+       //cout << "Message: " << pitchOSC << fixed << std::endl;
+    } // if
+    if(!msgpath.compare("/show_pitches")){
+       showPitch = argv[0]->i;
+       //cout << "Yes show me pitches" << showPitch << fixed << std::endl;
     } // if
 
     return 0;
@@ -33,8 +46,19 @@ void inputLijst()
 void outputLijst()
 {
   for (int i=0; i<numElements; i++)
-      cout << lijst[i] << std::endl;
+      cout << lijst[i] << " ";
 }
+
+void outputPitches()
+{
+  cout << "" << std::endl;
+  cout << "dit zit er in pitches array" << std::endl;
+  for (int i=0; i<aantal; i++)
+      cout << pitches[i] << " ";
+  cout << "" << std::endl;
+
+}
+
 
 int main()
 {
@@ -56,29 +80,64 @@ cout << "" << std::endl;
 
     osc.init(serverport);
     osc.set_callback("/velocity","i");
+    osc.set_callback("/kslider_pitch","i");
+    osc.set_callback("/show_pitches","i");
 
     osc.start();
     cout << "Listening on port " << serverport << endl;
 //einde OSC gebeuren
 
 int x=0;
+int y=0;
+int welkePitch=0;
 
 while(1)
 {
+  if(pitchOSC>0)
+  {
+    aantal++;
+    cout << "While message: " << pitchOSC << fixed << std::endl;
+    pitches[y]=pitchOSC;
+    y++;
+    pitchOSC=0;
+  }
+
+  if(showPitch==1)
+  {
+    outputPitches();
+    showPitch=0;
+  }
+
   //cout << params[0] << std::endl;
     if(velocityOSC>0)
     {
       cout << "" << std::endl;
       cout << lijst[x] << std::endl;
+      cout << pitches[welkePitch] << std::endl;
 
       x++;
+      welkePitch++;
 
         if(x>=numElements)
         x=0;
-        
+
+        if(welkePitch>=aantal)
+        welkePitch=0;
+
       velocityOSC=0;
     }
 }
 
 delete [] lijst;
+delete [] pitches;
 }
+
+/* git:
+git add (als er iets bij moet)
+git commit (als er wat verandert is)
+
+als het naar github moet:
+
+git push -u origin master
+
+*/
